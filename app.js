6912611d4c8ca11000121ba40ef2250c45c57df4
Cameron Glass <glass10@purdue.edu>
2017-06-07 16:07:15 -0400

    Set up submit handler for event form

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("JS Is Connected");
+const app = {
+    init(formSelector){
+        document
+            .querySelector(formSelector)
+            .addEventListener("submit", this.addDino);
+    },
+
+    addDino(ev){
+        ev.preventDefault();
+        const dinoName = ev.target.dinoName.value;
+        console.log(dinoName);
+    }
+}
+app.init("#dino-form");
