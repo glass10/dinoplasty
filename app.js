7fd289aa357f32be26218999779b8efc98889ecb
Cameron Glass <glass10@purdue.edu>
2017-06-08 15:23:04 -0400

    Fix arrow manipulation when refreshing and moving

--- app.js
+++ app.js
@@ -12,9 +12,31 @@ const app = {
         for(var j = 0; j < localStorage.length; j++){
             //localStorage.g
             this.dinos[j] = JSON.parse(localStorage.getItem(localStorage.key(j)));
-            const listItem = this.renderListItem(this.dinos[j]);
-            this.list.appendChild(listItem);
+            //const listItem = this.renderListItem(this.dinos[j]);
+            //this.list.appendChild(listItem);
         }
+        for(var j = 0; j < this.dinos.length; j++){
+                    const temp = JSON.parse(localStorage.getItem(this.dinos[j].name));
+                    if(j === 0){
+                        this.dinos[j].first = true;
+                            temp.first = true;
+                    }
+                    else{
+                        this.dinos[j].first = false;
+                            temp.first = false;
+                    }
+                    if(j === this.dinos.length-1){
+                        this.dinos[j].last = true;
+                            temp.last = true;
+                    }
+                    else{
+                        this.dinos[j].last = false;
+                            temp.last = false;
+                    }
+                    localStorage.setItem(this.dinos[j].name, JSON.stringify(temp));
+                    const listItem = this.renderListItem(this.dinos[j]);
+                    this.list.appendChild(listItem);
+                }
         console.log(this.dinos[0].name);
     },
 
@@ -99,7 +121,7 @@ const app = {
                 <div class="input-group-button" id="${dino.name}">
                     ${upButton}
                     ${downButton}
-                    <input type="button" id="${starLabel}" class="success button" onClick=app.star(this.parentNode.id) value="★">
+                    <input type="button" id="${starLabel}" class="warning button" onClick=app.star(this.parentNode.id) value="★">
                     <input type="button" id="delete" class="alert button" onClick=app.delete(this.parentNode.id) value="✗">
                 </div>
         `
@@ -126,6 +148,24 @@ const app = {
                 app.list.innerHTML = "";
 
                 for(var j = 0; j < this.dinos.length; j++){
+                    const temp = JSON.parse(localStorage.getItem(this.dinos[j].name));
+                    if(j === 0){
+                        this.dinos[j].first = true;
+                            temp.first = true;
+                    }
+                    else{
+                        this.dinos[j].first = false;
+                            temp.first = false;
+                    }
+                    if(j === this.dinos.length-1){
+                        this.dinos[j].last = true;
+                            temp.last = true;
+                    }
+                    else{
+                        this.dinos[j].last = false;
+                            temp.last = false;
+                    }
+                    localStorage.setItem(this.dinos[j].name, JSON.stringify(temp));
                     const listItem = this.renderListItem(this.dinos[j]);
                     this.list.appendChild(listItem);
                 }
@@ -147,6 +187,24 @@ const app = {
                 app.list.innerHTML = "";
 
                 for(var j = 0; j < this.dinos.length; j++){
+                    const temp = JSON.parse(localStorage.getItem(this.dinos[j].name));
+                    if(j === 0){
+                        this.dinos[j].first = true;
+                            temp.first = true;
+                    }
+                    else{
+                        this.dinos[j].first = false;
+                            temp.first = false;
+                    }
+                    if(j === this.dinos.length-1){
+                        this.dinos[j].last = true;
+                            temp.last = true;
+                    }
+                    else{
+                        this.dinos[j].last = false;
+                            temp.last = false;
+                    }
+                    localStorage.setItem(this.dinos[j].name, JSON.stringify(temp));
                     const listItem = this.renderListItem(this.dinos[j]);
                     this.list.appendChild(listItem);
                 }
